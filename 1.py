21779973b1f4d55c61f0a359979045b0bbfc6365
FSoslaNE <frievsoslan@gmail.com>
2026-05-10 21:59:43 +0300

    Added print result

--- 1.py
+++ 1.py
@@ -4,6 +4,10 @@ def main():
     sum_result = a + b
     diff_result = a - b
     mult_result = a * b
+    print(f"{a} + {b} = {sum_result}")
+    print(f"{a} - {b} = {diff_result}")
+    print(f"{a} * {b} = {mult_result}")
+
 
 if __name__ == __main__:
     main()
